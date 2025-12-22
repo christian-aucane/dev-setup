8757de7d3ac1315948bec97e86bc3ff97119a8f1
Christian Aucane <christianaucane1@gmail.com>
2025-12-22 23:41:11 +0100

    create clean structure

---
+++ scripts/dotfiles.py
@@ -0,0 +1,28 @@
+#! TODO: ajouter path vers python
+import argparse
+import sys
+
+
+def link():
+    ...
+
+
+def install():
+    ...
+
+
+def update():
+    ...
+
+
+def check():
+    ...
+
+
+def run():
+    args = _parse_args(sys.argv)
+    ...
+
+
+if __name__ == "__main__":
+    sys.exit(run())
